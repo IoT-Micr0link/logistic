5b78f94d93b56a3868e6c28e1e96256263e97acb
Julian Vega <jcvgalfonso@gmail.com>
2020-02-28 12:17:39 -0500

    feat: Add total items to  IoT main dashbaord

--- iot/views.py
+++ iot/views.py
@@ -1,5 +1,11 @@
 from django.views import generic
+from rfid.models import Item
 
 
 class DashboardView(generic.TemplateView):
     template_name = 'dashboard/logistica/index_logistica.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['total_items'] = Item.objects.all().count()
+        return context
